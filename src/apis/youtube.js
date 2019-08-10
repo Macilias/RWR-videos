1172b9779ad10012892af8156dfe6dfd100d6b07
Maciek Niemczyk <Maciek.Niemczyk@vitruam.de>
2019-08-10 10:50:32 +0200

    init 0
    init 2 polish
    
    init 3 ups
    
    init 3 ups
    
    shit git

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDZZRxjGwcBJ6JMAN0Ci8Pk7NGMoP2mU94';
+const KEY = 'secret';
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
